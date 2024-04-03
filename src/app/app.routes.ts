66f110264141f86edd3dc24f39e225399dca83c3
Siddhesh Desai (MINDTREE LIMITED) <v-sidesai@microsoft.com>
2024-04-03 12:02:23 +0530

    first commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { OrdersComponent } from './orders/orders.component';
+
+const routes: Routes = [
+  { path: 'orders', component: OrdersComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
+export {routes};
+
